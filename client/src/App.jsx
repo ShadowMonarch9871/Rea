da70a2179180ced49e6834c1137e45115f993927
Ayush Pokhriyal <ayushpokhriyal21@gmail.com>
2023-12-06 14:52:19 +0530

    Search functionality done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
 import UpdateListing from "./pages/UpdateListing";
 import Listing from "./pages/Listing";
+import Search from "./pages/Search";
 
 const App = () => {
   return (
@@ -18,6 +19,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/search" element={<Search />} />
+
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route element={<PrivateRoute />}>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,29 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { TbHomeSearch } from "react-icons/tb";
 import { useSelector } from "react-redux";
 
 const Header = () => {
-  const {currentUser}  = useSelector((state) => state.user);
+  const { currentUser } = useSelector((state) => state.user);
   if (currentUser) {
     console.log(currentUser);
   }
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -17,9 +33,14 @@ const Header = () => {
             <span className="text-slate-700">Estate</span>
           </h1>
         </Link>
-        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-slate-100 p-3 rounded-lg flex items-center"
+        >
           <input
             type="text"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
             placeholder="Search Property"
             className="bg-transparent w-24 sm:w-64 focus:outline-none"
           />
@@ -34,7 +55,11 @@ const Header = () => {
           </Link>{" "}
           <Link to="/profile">
             {currentUser ? (
-              <img src={currentUser.avatar} alt="User Avatar" className="h-7 w-7 rounded-full " />
+              <img
+                src={currentUser.avatar}
+                alt="User Avatar"
+                className="h-7 w-7 rounded-full "
+              />
             ) : (
               <li className="text-slate-700 hover:underline">Sign In</li>
             )}
