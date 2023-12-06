b20bb30e409b06997716b6a62ab4f74e403b6f0c
Ayush Pokhriyal <ayushpokhriyal21@gmail.com>
2023-12-06 14:08:07 +0530

    Deleting Listing Added

--- server/controllers/listing.controller.js
+++ server/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 export const createListing = async (req, res, next) => {
   try {
     const listing = await Listing.create(req.body);
@@ -7,4 +8,19 @@ export const createListing = async (req, res, next) => {
     next(error);
   }
   // res.json({ message: "Routing is Working😎" });
+};
+export const deleteListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, "Listing Not Found"));
+  }
+  if (req.user.id != listing.userRef) {
+    return next(errorHandler(401, "You can only delete your own listing"));
+  }
+  try {
+    await Listing.findByIdAndDelete(req.params.id);
+    res.status(200).json("Listing Deleted");
+  } catch (e) {
+    next(e);
+  }
 };
